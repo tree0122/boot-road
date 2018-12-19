fd00d8b4d5c6047c52115107d6f1d6d74837ad3d
cuiyaohua <cuiyaohua@mobike.com>
2018-12-19 19:35:03 +0800

    @resource multi bean

---
+++ src/main/java/com/tree/bootroad/multibean/MultiConfig.java
@@ -0,0 +1,23 @@
+package com.tree.bootroad.multibean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MultiConfig {
+
+    @Bean
+    public MultiService multiService(){
+        return new MultiService();
+    }
+
+    @Bean
+    public Service0 service0(){
+        return new Service0();
+    }
+
+    @Bean
+    public Service0 service1(){
+        return new Service0();
+    }
+}
---
+++ src/main/java/com/tree/bootroad/multibean/MultiService.java
@@ -0,0 +1,9 @@
+package com.tree.bootroad.multibean;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+public class MultiService {
+    @Resource
+    private List<Service0> service0List;
+}
