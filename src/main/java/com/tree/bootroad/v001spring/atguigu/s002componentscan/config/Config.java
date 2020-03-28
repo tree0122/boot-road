cf06d4e4f196b10ef175f781e6379a965621a384
tree0122 <cuiyaohua11@163.com>
2020-03-28 21:27:04 +0800

    agguigu: spring bean component scan, comment

--- src/main/java/com/tree/bootroad/v001spring/atguigu/s002componentscan/config/Config.java
+++ src/main/java/com/tree/bootroad/v001spring/atguigu/s002componentscan/config/Config.java
@@ -5,6 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Controller;
 
+/**
+ * @ComponentScan:
+ * value = "" 指定要扫描的包
+ * excludeFilters = Filter[], 指定扫描时按照规则排除哪些组件
+ * includeFilters = Filter[], 指定扫描时按照规则只需要哪些组件
+ * useDefaultFilters = bool, 是否启用默认规则
+ *
+ * @ComponentScans: 多个 @ComponentScan
+ */
 @Configuration
 @ComponentScan(value = "com.tree.bootroad.v001spring.atguigu.s002componentscan", excludeFilters = {
         @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class})
