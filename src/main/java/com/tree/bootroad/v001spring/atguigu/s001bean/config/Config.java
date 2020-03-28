c75f0ae96c32870f1e49c826e1f5bef50ee14c6e
tree0122 <cuiyaohua11@163.com>
2020-03-28 21:17:46 +0800

    agguigu: spring bean component, comment

--- src/main/java/com/tree/bootroad/v001spring/atguigu/s001bean/config/Config.java
+++ src/main/java/com/tree/bootroad/v001spring/atguigu/s001bean/config/Config.java
@@ -4,9 +4,18 @@ import com.tree.bootroad.v001spring.atguigu.s001bean.bean.Person;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @Configuration 告诉spring这是一个配置类
+ *
+ * 配置类, 也就是配置文件(xml)
+ */
 @Configuration
 public class Config {
 
+    /**
+     * @Bean给容器注册一个Bean: 类型为返回的类型, id默认是方法名
+     * @return
+     */
     @Bean
     public Person person(){
         return new Person(18, "best");
